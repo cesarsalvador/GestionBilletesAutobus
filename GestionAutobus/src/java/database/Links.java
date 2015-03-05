2fd4d272dcfa221791a43270458d4d9e4f8b36d8
usuario0316 <usuario0316@aula0316>
2015-03-05 11:29:14 +0100

    Subo los archivos iniciales del proyecto

---
+++ GestionAutobus/src/java/database/Links.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author usuario0316
+ */
+public class Links {
+    private String ref;
+    private String ruta;
+
+    /**
+     * @return the ref
+     */
+    public String getRef() {
+        return ref;
+    }
+
+    /**
+     * @param ref the ref to set
+     */
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    /**
+     * @return the ruta
+     */
+    public String getRuta() {
+        return ruta;
+    }
+
+    /**
+     * @param ruta the ruta to set
+     */
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    
+}
